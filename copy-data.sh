9d4ba81c70261950f10e59fb60c04498ecd25a8d
aubreymoore <aubreymoore@guam.net>
2020-11-21 20:25:01 +1000

    edited copy-data.sh

--- copy-data.sh
+++ copy-data.sh
@@ -1,14 +1,25 @@
-cd /home/aubrey/Desktop/new-crb-damage-map
+# Usage: ./copy-data.sh
+
+# clone the repo onto the Desktop and change into the new directory
+
+set -e
+cd ~/Desktop
+git clone https://github.com/aubreymoore/new-crb-damage-map.git
+cd new-crb-damage-map
 
 # Copy files into a new folder, original-map
 
 mkdir original-map
-cd original-map
-cp /home/aubrey/Desktop/Guam-CRB-damage-map-2020-10/Guam01.db .
-cp /home/aubrey/Desktop/Guam-CRB-damage-map-2020-10/make_crb_damage_map.py .
-cp /home/aubrey/Desktop/Guam-CRB-damage-map-2020-10/edit_webmap.py .
-cd /home/aubrey/Desktop/new-crb-damage-map
+cp /home/aubrey/Desktop/Guam-CRB-damage-map-2020-10/Guam01.db original-map/.
+cp /home/aubrey/Desktop/Guam-CRB-damage-map-2020-10/make_crb_damage_map.py original-map/.
+cp /home/aubrey/Desktop/Guam-CRB-damage-map-2020-10/edit_webmap.py original-map/.
 
-# Save a copy of original-map in new-map
+# Save a copy of original-map as new-map
 
 cp -R original-map new-map
+
+# Make map in original-map
+
+cd original-map
+qgis --code make_crb_damage_map.py
+
